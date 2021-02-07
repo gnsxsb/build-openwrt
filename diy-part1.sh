4c7be92d68af42a3ef3e7cd27e49d43f4d87479d
1553800919 <69191743+1553800919@users.noreply.github.com>
2021-02-07 23:10:19 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,9 +1,6 @@
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
 # poweroff
 git clone https://github.com/esirplayground/luci-app-poweroff.git package/my-package/luci-app-poweroff
 
