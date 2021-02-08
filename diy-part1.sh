e1d9b154f4b5cb0e08a191f11d976f5838a1df35
1553800919 <69191743+1553800919@users.noreply.github.com>
2021-02-08 10:11:28 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -3,10 +3,7 @@
 git clone https://github.com/fw876/helloworld.git package/my-package/helloworld
 
 # poweroff
-# git clone https://github.com/esirplayground/luci-app-poweroff.git package/my-package/luci-app-poweroff
-
-# poweroffdevice
-git clone https://github.com/sirpdboy/luci-app-poweroffdevice package/my-package/luci-app-poweroffdevice
+git clone https://github.com/esirplayground/luci-app-poweroff.git package/my-package/luci-app-poweroff
 
 # argon主题
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/my-package/luci-theme-argon
@@ -25,9 +22,6 @@ git clone https://github.com/lisaac/luci-lib-docker.git package/my-package/luci-
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/my-package/luci-app-adguardhome
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-adguardhome package/my-package/luci-app-adguardhome
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/adguardhome package/my-package/adguardhome
-
-# controlweburl
-svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-control-weburl package/my-package/luci-app-control-weburl
 
 # turboacc
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-turboacc package/my-package/luci-app-turboacc
