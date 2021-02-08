12a438b06174ecf5be9e0ce39127e3045cb5c411
1553800919 <69191743+1553800919@users.noreply.github.com>
2021-02-08 21:28:57 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,21 +8,16 @@ git clone https://github.com/esirplayground/luci-app-poweroff.git package/my-pac
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/my-package/luci-theme-argon
 
 # smartdns
-svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-smartdns package/my-package/luci-app-smartdns
-svn co https://github.com/sirpdboy/sirpdboy-package/trunk/smartdns package/my-package/smartdns
-#git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/my-package/luci-app-smartdns
-#git clone https://github.com/pymumu/openwrt-smartdns.git package/my-package/smartdns
-
-# control-weburl
-svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-control-weburl package/my-package/luci-app-control-weburl
+git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/my-package/luci-app-smartdns
+git clone https://github.com/pymumu/openwrt-smartdns.git package/my-package/smartdns
 
 # dockerman
 git clone https://github.com/lisaac/luci-app-dockerman.git package/my-package/luci-app-dockerman
 git clone https://github.com/lisaac/luci-lib-docker.git package/my-package/luci-lib-docker
 
 # adguardhome
-svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-adguardhome package/my-package/luci-app-adguardhome
-svn co https://github.com/sirpdboy/sirpdboy-package/trunk/adguardhome package/my-package/adguardhome
+# svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-adguardhome package/my-package/luci-app-adguardhome
+# svn co https://github.com/sirpdboy/sirpdboy-package/trunk/adguardhome package/my-package/adguardhome
 
 # turboacc
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-turboacc package/my-package/luci-app-turboacc
