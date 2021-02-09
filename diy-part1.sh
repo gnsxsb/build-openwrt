8ef15c6f6002ede2f342a8e932cab01882feede0
1553800919 <69191743+1553800919@users.noreply.github.com>
2021-02-09 22:00:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -2,24 +2,24 @@
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # poweroff
-git clone https://github.com/esirplayground/luci-app-poweroff.git package/my-package/luci-app-poweroff
+git clone https://github.com/esirplayground/luci-app-poweroff.git package/lean/luci-app-poweroff
 
 # argon主题
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/my-package/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 
 # smartdns
-git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/my-package/luci-app-smartdns
-git clone https://github.com/pymumu/openwrt-smartdns.git package/my-package/smartdns
+git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns
+git clone https://github.com/pymumu/openwrt-smartdns.git package/lean/smartdns
 
 # dockerman
-git clone https://github.com/lisaac/luci-app-dockerman.git package/my-package/luci-app-dockerman
-git clone https://github.com/lisaac/luci-lib-docker.git package/my-package/luci-lib-docker
+git clone https://github.com/lisaac/luci-app-dockerman.git package/lean/luci-app-dockerman
+# git clone https://github.com/lisaac/luci-lib-docker.git package/lean/luci-lib-docker
 
 # adguardhome
-git clone https://github.com/kongfl888/luci-app-adguardhome.git package/my-package/luci-app-adguardhome
+git clone https://github.com/kongfl888/luci-app-adguardhome.git package/lean/luci-app-adguardhome
 
 # wrtbwmon-zhcn
-git clone https://github.com/firker/luci-app-wrtbwmon-zh.git package/my-package/luci-app-wrtbwmon-zhcn
+git clone https://github.com/firker/luci-app-wrtbwmon-zh.git package/lean/luci-app-wrtbwmon-zhcn
 
 # ttnode
-git clone https://github.com/jerrykuku/luci-app-ttnode.git package/my-package/luci-app-ttnode
+git clone https://github.com/jerrykuku/luci-app-ttnode.git package/lean/luci-app-ttnode
